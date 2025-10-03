9830efa025101fb9a106ed2746b6d4c7109a9929
ammaralk1 <ammar.director@gmail.com>
2025-10-03 17:45:45 +0300

    Updates

---
+++ js/godaddy-monitor.js
@@ -0,0 +1,15 @@
+/* GoDaddy performance beacon */
+if (typeof window._trfq === 'undefined') {
+  window._trfq = [];
+}
+if (typeof window._trfd === 'undefined') {
+  window._trfd = [];
+}
+window._trfd.push(
+  { 'tccl.baseHost': 'secureserver.net' },
+  { ap: 'cpsh-oh' },
+  { server: 'sxb1plzcpnl453515' },
+  { dcenter: 'sxb1' },
+  { cp_id: '7619770' },
+  { cp_cl: '8' }
+);
