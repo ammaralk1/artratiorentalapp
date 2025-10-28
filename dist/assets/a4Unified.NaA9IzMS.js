e9867bf1418673b524d42d37cc1a39c2ad3f152d
ammaralk1 <ammar.director@gmail.com>
2025-10-28 23:48:29 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/a4Unified.NaA9IzMS.js
+++ dist/assets/a4Unified.NaA9IzMS.js
@@ -1,4 +1,4 @@
-import{r as P,t as o,e as F,d as T,b as E,p as M}from"./calculations.DHt01fQF.js";import{e as H,l as L,a as X,b as G}from"./reports.MQ4BsIdo.js";import"./reservationsService.GmbcfUDz.js";import"./auth.AnAB4TiF.js";import"./dashboard.DiJAYeIc.js";import"./controller.TcMuw5pZ.js";/* empty css              */import"./dashboardShell.CXfzZqjK.js";import"./customers.DyLSucpV.js";import"./maintenanceService.B1OwjilU.js";const W=`@page { size: A4; margin: 0; }
+import{r as P,t as o,e as F,d as T,b as E,p as M}from"./calculations.CjZXnTPP.js";import{e as H,l as L,a as X,b as G}from"./reports.CxZxSYQU.js";import"./reservationsService.B2-wDlNQ.js";import"./auth.B3hKYgQo.js";import"./dashboard.DZnS8WVM.js";import"./controller.Cb-ucNZb.js";/* empty css              */import"./dashboardShell.C0SmW5cH.js";import"./customers.CvBVNDhF.js";import"./maintenanceService.QI89zx-j.js";const W=`@page { size: A4; margin: 0; }
 @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;600;700;800&display=swap');
 
 /* جذر وثيقة A4 للمعاينة/التصدير */
