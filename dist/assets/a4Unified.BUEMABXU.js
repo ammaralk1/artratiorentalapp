7805a34b51b9a5495bdf041650168d04ee3aed45
ammaralk1 <ammar.director@gmail.com>
2025-11-01 02:22:12 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/a4Unified.BUEMABXU.js
+++ dist/assets/a4Unified.BUEMABXU.js
@@ -1,4 +1,4 @@
-import{r as _,t as s,h as T,f as L,d as C,p as q}from"./calculations.CRYbyPCe.js";import{e as H,l as A,a as X,b as B}from"./reports.CIuQK0uD.js";import"./reservationsService.CGyV-C6u.js";import"./auth.BFR8Y3ym.js";import"./state.uepyuVVl.js";import"./preload-helper.PPVm8Dsz.js";import"./maintenanceService.CQt6-S02.js";import"./canvasColorUtils.CviLtv6S.js";const G=`@page { size: A4; margin: 0; }
+import{r as _,t as s,h as T,f as L,d as C,p as q}from"./calculations.rH_4djUn.js";import{e as H,l as A,a as X,b as B}from"./reports.g6cgypsp.js";import"./reservationsService.BOHdxBWH.js";import"./auth.BFR8Y3ym.js";import"./state.uepyuVVl.js";import"./preload-helper.PPVm8Dsz.js";import"./maintenanceService.CQt6-S02.js";import"./canvasColorUtils.CviLtv6S.js";const G=`@page { size: A4; margin: 0; }
 @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;600;700;800&display=swap');
 
 /* جذر وثيقة A4 للمعاينة/التصدير */
