349890828835ec15449aa4d78159fd01930c7872
ammaralk1 <ammar.director@gmail.com>
2025-11-05 18:37:24 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/preview.BtTqEizf.js
+++ dist/assets/preview.BtTqEizf.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.D936Ulyo.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.BQm12kbh.js";import"./reservationsService.BIddNe5f.js";import"./auth.CIDuDiYC.js";import"./state.LA28UGJ5.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.DKYVJEo5.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.BqFFuyUh.js";import"./reservationsService.DbKtl5I6.js";import"./auth.Co_dIG69.js";import"./state.CMNuYVcO.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
