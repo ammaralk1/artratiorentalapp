2c2742fc426d27d9c705ea17c4ad58bea1072ef1
ammaralk1 <ammar.director@gmail.com>
2025-11-08 03:32:44 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/preview.3TUBA98B.js
+++ dist/assets/preview.3TUBA98B.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.w4ptIcQG.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.CB5sHh6I.js";import"./reservationsService.Bgn2pRxd.js";import"./auth.BlcY7Yb5.js";import"./state.DRcArL_g.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.DHt-nfUZ.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.Bt2w6dfz.js";import"./reservationsService.Bt3L-JTg.js";import"./auth.LlfMNK-p.js";import"./state.BNQuyYRR.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
