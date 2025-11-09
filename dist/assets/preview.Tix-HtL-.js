4ba0c4dd0935709f38871341f505209ef99b5a26
ammaralk1 <ammar.director@gmail.com>
2025-11-09 04:05:20 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/preview.Tix-HtL-.js
+++ dist/assets/preview.Tix-HtL-.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.DWxjbU9W.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.CRRcSiyt.js";import"./reservationsService.BGgi72ed.js";import"./auth.Dym-pMl4.js";import"./state.D0_ft4YH.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.BVcyzysX.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.CEvT-xX-.js";import"./reservationsService.BGgi72ed.js";import"./auth.Dym-pMl4.js";import"./state.D0_ft4YH.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
