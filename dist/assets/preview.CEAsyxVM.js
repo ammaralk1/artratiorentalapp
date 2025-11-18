686bcccd38c6f55cf05369a2d5387e87f3ca3bd8
ammaralk1 <ammar.director@gmail.com>
2025-11-18 20:35:56 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/preview.CEAsyxVM.js
+++ dist/assets/preview.CEAsyxVM.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.pIPUx5GN.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.B9-0zFnU.js";import"./reservationsService.DsLJB5GZ.js";import"./auth.BLFzWQsL.js";import"./state.BK2rhNHf.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.4gbbmNEp.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.CdFctv9F.js";import"./reservationsService.D0xTETx8.js";import"./auth.BLFzWQsL.js";import"./state.BK2rhNHf.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
