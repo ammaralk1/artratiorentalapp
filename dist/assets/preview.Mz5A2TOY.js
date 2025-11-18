5473d25197a5033696eaaf833b6963083a98cdee
ammaralk1 <ammar.director@gmail.com>
2025-11-19 01:39:12 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/preview.Mz5A2TOY.js
+++ dist/assets/preview.Mz5A2TOY.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.TtbAnx5N.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.D61Egi8B.js";import"./reservationsService.BARViMdt.js";import"./auth.BLFzWQsL.js";import"./state.BK2rhNHf.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.D9_62hu6.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.B9BhEesf.js";import"./reservationsService.C1x_67yQ.js";import"./auth.BLFzWQsL.js";import"./state.BK2rhNHf.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
