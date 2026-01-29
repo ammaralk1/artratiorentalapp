0f1e0d24bd9ca3c718110d7ae1314843fbdc7535
ammaralk1 <ammar.director@gmail.com>
2026-01-29 19:16:18 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/a4Unified.YZCfmmst.js
+++ dist/assets/a4Unified.YZCfmmst.js
@@ -1,4 +1,4 @@
-import{r as _,t as s,e as q,l as A,k as I,g as L,h as C,p as H,a as X,K as G}from"./calculations.wlhRUAA4.js";import"./reservationsService.CKBqxPIy.js";import"./auth.CJ8hUxus.js";import"./technicians.CkrqkAg4.js";const B=`@page { size: A4; margin: 0; }
+import{r as _,t as s,e as q,l as A,k as I,g as L,h as C,p as H,a as X,K as G}from"./calculations.C_dSDhuC.js";import"./reservationsService.BKnSnh-T.js";import"./auth.CS33KJXJ.js";import"./technicians.CVAfs_kF.js";const B=`@page { size: A4; margin: 0; }
 @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;600;700;800&display=swap');
 
 /* جذر وثيقة A4 للمعاينة/التصدير */
