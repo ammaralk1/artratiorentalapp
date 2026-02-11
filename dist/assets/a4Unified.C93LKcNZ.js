a103fd9b0c65c54c671868c54d0f32f70884e411
ammaralk1 <ammar.director@gmail.com>
2026-02-11 14:37:58 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/a4Unified.C93LKcNZ.js
+++ dist/assets/a4Unified.C93LKcNZ.js
@@ -1,4 +1,4 @@
-import{r as _,t as s,e as q,l as A,k as I,g as L,h as C,p as H,a as X,K as G}from"./calculations.ynBjO_CY.js";import"./reservationsService.Bz_TAegX.js";import"./auth.Bccf-xsM.js";import"./technicians.BROQVNHZ.js";const B=`@page { size: A4; margin: 0; }
+import{r as _,t as s,e as q,l as A,k as I,g as L,h as C,p as H,a as X,K as G}from"./calculations.C8eyJRj0.js";import"./reservationsService.BZLG_R6A.js";import"./auth.Bccf-xsM.js";import"./technicians.BROQVNHZ.js";const B=`@page { size: A4; margin: 0; }
 @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;600;700;800&display=swap');
 
 /* جذر وثيقة A4 للمعاينة/التصدير */
--- dist/assets/preview.Dm5GkgeI.js
+++ dist/assets/preview.Dm5GkgeI.js
@@ -1,4 +1,4 @@
-import{r as T,t as L}from"./calculations.ynBjO_CY.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.BIzNI3c5.js";import"./reservationsService.Bz_TAegX.js";import"./auth.Bccf-xsM.js";import"./technicians.BROQVNHZ.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
+import{r as T,t as L}from"./calculations.C8eyJRj0.js";import{buildA4ReportPages as $,exportA4ReportPdf as _}from"./a4Unified.C93LKcNZ.js";import"./reservationsService.BZLG_R6A.js";import"./auth.Bccf-xsM.js";import"./technicians.BROQVNHZ.js";function H(){const l=document.createElement("div");return l.className="modal fade quote-preview-modal",l.setAttribute("tabindex","-1"),l.setAttribute("aria-hidden","true"),l.innerHTML=`
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
