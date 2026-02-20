d6197fdb939411e9323b279711b0359eb00fa1cc
ammaralk1 <ammar.director@gmail.com>
2026-02-20 23:32:51 +0300

    Switch bundled logo to AR-Logo-v3.5-curved.png

--- dist/assets/AR-Logo-v3.5-curved-WH.4CfchrKg.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("AR-Logo-v3.5-curved-WH.BkqPNgzF.png",import.meta.url).href;export{o as q};
---
+++ dist/assets/AR-Logo-v3.5-curved-WH.BONvRdJv.js
@@ -0,0 +1,1 @@
+const o=""+new URL("AR-Logo-v3.5-curved-WH.DRFh0NTm.png",import.meta.url).href;export{o as q};
