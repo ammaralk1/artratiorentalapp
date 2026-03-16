1b2de02704a7ce1045da9e5915c2cd0ca0ca5afc
ammaralk1 <ammar.director@gmail.com>
2026-03-17 02:39:36 +0300

    chore(build): update dist [skip-hook]

--- dist/assets/a4Unified.DfnoHMuF.js
+++ dist/assets/a4Unified.DfnoHMuF.js
@@ -1,4 +1,4 @@
-import{r as _,t as s,e as H,l as F,k as T,g as L,h as C,p as X,a as B,K as G}from"./calculations.C8eyJRj0.js";import{q as W}from"./AR-Logo-v3.5-curved-WH.BONvRdJv.js";import"./reservationsService.BZLG_R6A.js";import"./auth.Bccf-xsM.js";import"./technicians.BROQVNHZ.js";const J=`@page { size: A4; margin: 0; }
+import{r as _,t as s,e as H,l as F,k as T,g as L,h as C,p as X,a as B,K as G}from"./calculations.Bg39hi5z.js";import{q as W}from"./AR-Logo-v3.5-curved-WH.BONvRdJv.js";import"./reservationsService.altaKvwE.js";import"./auth.DTeKfqCQ.js";import"./technicians.CQaz9WUk.js";const J=`@page { size: A4; margin: 0; }
 @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;600;700;800&display=swap');
 
 /* جذر وثيقة A4 للمعاينة/التصدير */
